7aeacd95f09c6ebb5180e7a4a1d6edca55331c6f
Hermann <adolfo1349@gmail.com>
2022-04-04 21:18:29 -0300

    Js code update

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-var $stoneButton = document.querySelector(".button-stone-player-1");
-var $fieldPlayer1 = document.querySelector(".field-player-1");
+const $stoneButton = document.querySelector(".button-stone-player-1");
+const $paperButton = document.querySelector(".button-paper-player-1");
+const $scissorsButton = document.querySelector(".button-scissors-player-1");
 
-function movePlayer1Stone() {
-  $fieldPlayer1.innerHTML = '<img src="./Assets/stone.png" class="stone-hand">';
-}
+const $fieldPlayer1 = document.querySelector(".field-player-1");
 
-$stoneButton.addEventListener("click", movePlayer1Stone);
+$stoneButton.addEventListener("click", function () {
+  $fieldPlayer1.innerHTML = '<img src="./Assets/stone.png" class="move-image">';
+});
+
+$paperButton.addEventListener("click", function () {
+    $fieldPlayer1.innerHTML = '<img src="./Assets/paper.png" class="move-image">';
+  });
+
+  $scissorsButton.addEventListener("click", function () {
+    $fieldPlayer1.innerHTML = '<img src="./Assets/scissors.png" class="move-image">';
+  });
